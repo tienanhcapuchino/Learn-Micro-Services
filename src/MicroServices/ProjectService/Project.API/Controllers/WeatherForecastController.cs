22e864e89c3492a7d2825995bfa34e13f5baf4b2
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-12 22:11:36 +0700

    fix gateway

--- src/MicroServices/ProjectService/Project.API/Controllers/WeatherForecastController.cs
+++ src/MicroServices/ProjectService/Project.API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Project.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("~/project-api/[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
