4de7364702f1853aaf33fc39a8478e602025c567
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-14 00:15:01 +0700

    add use swagger UI for api gateway

--- src/APIGateway/Gateway.API/Startup.cs
+++ src/APIGateway/Gateway.API/Startup.cs
@@ -32,6 +32,11 @@ namespace Gateway.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            else
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
