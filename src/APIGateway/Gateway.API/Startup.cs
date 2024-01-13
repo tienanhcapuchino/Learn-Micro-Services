f17939f1988cd0f50d9402bcca397d3a7bf6051a
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-13 21:47:06 +0700

    reconfigure gateway

---
+++ src/APIGateway/Gateway.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup()
+        {
+            this.Configuration = new ConfigurationBuilder()
+                .AddJsonFile("ocelot.json")
+                .Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddOcelot(Configuration);
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseOcelot();
+        }
+    }
+}
