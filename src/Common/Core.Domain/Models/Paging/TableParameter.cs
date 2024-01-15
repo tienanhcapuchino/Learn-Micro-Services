1d56e51051f410f68f0c4bf16e80cdaaa220dcba
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-15 22:14:53 +0700

    add core service

---
+++ src/Common/Core.Domain/Models/Paging/TableParameter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Models
+{
+    public class TableParameter
+    {
+        public int Page { get; set; }
+        public int Top { get; set; }
+        public int Skip { get; set; }
+        public string? SearchContent { get; set; }
+        public List<string>? SearchByColumns { get; set; }
+        public PageRequestOrderBy? OrderBy { get; set; }
+        public IEnumerable<PageRequestFilter>? Filter { get; set; }
+        public void AddAdditionalFilters(IEnumerable<PageRequestFilter> additionalFilter)
+        {
+            var filter = Filter?.ToList();
+            if (filter is null)
+            {
+                filter = new List<PageRequestFilter>();
+            }
+            filter.AddRange(additionalFilter);
+            Filter = filter;
+        }
+    }
+
+    public class PageRequestOrderBy
+    {
+        public bool IsAscending { get; set; }
+        public string OrderByKey { get; set; }
+    }
+
+    public class PageRequestFilter
+    {
+        private IEnumerable<string> _value;
+        public string ColumnName { get; set; }
+        public bool IsNullValue { get; set; }
+        public IEnumerable<string> Value
+        {
+            get { return this.IsNullValue ? new List<string>() : _value; }
+            set
+            {
+                _value = value;
+            }
+        }
+        public bool IncludeNullValue { get; set; } = false;
+    }
+
+}
---
+++ src/Common/Core.Service/Services/CoreDbRepository.cs
@@ -0,0 +1,37 @@
+﻿using Core.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service.Services
+{
+    public class CoreDbRepository<T> : ICoreDbRepository<T>
+    {
+        public Task<bool> AddAync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
