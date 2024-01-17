8144d8412dd26c9bced39f36db7cc96fbe3c4455
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-17 22:44:03 +0700

    try to use AWS S3 to store files

---
+++ src/MicroServices/CommonService/Common.Domain/Models/GlobalSettings/GlobalSettingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Models
+{
+    public class FileS3StorageSetting
+    {
+        public string AWSKey { get; set; } = "";
+        public string AWSSecretKey { get; set; } = "";
+    }
+}
