4af6de95a00cc8af30d8b67db784d9e83702ef9a
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2024-01-20 20:58:30 +0700

    configure web MVC with react

--- src/MicroServices/ProjectService/Project.API/Program.cs
+++ src/MicroServices/ProjectService/Project.API/Program.cs
@@ -1,26 +1,14 @@
 using Core.Domain.Constants;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+using Project.API;
 
 Console.Title = ApplicationHostName.Project;
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration); // My custom startup class.
+
+startup.ConfigureServices(builder.Services); // Add services to the container.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment); // Configure the HTTP request pipeline.
 
 app.Run();
---
+++ src/Web/LearnWeb/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
+namespace LearnWeb
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            //make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName).AddV8();
+            services.AddControllersWithViews();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseReact(config =>
+            {
+                //config.
+            });
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
